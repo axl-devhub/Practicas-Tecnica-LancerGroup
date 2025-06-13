ba5727d5de4737f022b69ac3603c994db0984be4
axl-devhub <hideoka1176@gmail.com>
2025-06-13 12:48:36 -0400

    fix: update home route to use BookController for rendering books

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Books');
-})->name('home');
+Route::get('/', [BookController::class, 'index'])->name('home');
+Route::resource('books', BookController::class);
 
 Route::resource('authors', AuthorController::class);
